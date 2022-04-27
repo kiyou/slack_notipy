0b6bf81b4a57e49162d3a08be15b2992717d56f8
kiyou <46545721+kiyou@users.noreply.github.com>
2022-04-27 20:54:30 +0900

    [add] add setup.cfg and __version__.py and modify setup.py (#1)

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='slack_notipy',
-    version="0.1.0",
-    description="A simple script for sending decorated notifications using Slack Incoming Webhook from Python3.",
-    long_description="A simple script for sending decorated notifications using Slack Incoming Webhook from Python3.",
-    url='https://github.com/kiyou/slack_notipy',
-    author='kiyou',
-    author_email='',
-    license='MIT',
-    classifiers=[
-        # https://pypi.python.org/pypi?:action=list_classifiers
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: MIT License",
-    ],
-    keywords='tools',
-    install_requires=["python-dotenv",],
-    py_modules=["slack_notipy"],
-    packages=find_packages("."),
-    include_package_data=True,
-    package_data={
-        "slack_notipy": ["config.json"]
-    },
-    entry_points={
-        'console_scripts':[
-            'slack_notipy = slack_notipy:cli',
-        ],
-    },
-)
+setup()
